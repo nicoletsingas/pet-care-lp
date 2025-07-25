b58cd4584491dfb6cbdec0eaabb47eb9b1bf03e0
Nicole Tsingas <nicolecass27@gmail.com>
2025-07-24 21:05:58 -0300

    feat: criado tela de serviços, testemunhos e footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import Sobre from "@/components/Sobre";
+import { Servicos } from "@/components/Servicos";
+import { Testemunhos } from "@/components/Testemunhos";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
     return (
         <main>
             <Hero />
             <Sobre />
+            <Servicos />
+            <Testemunhos />
+            <Footer />
         </main>
     );
 }
